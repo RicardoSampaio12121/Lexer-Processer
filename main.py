25b469aeb638f35bd0d9a46b60f2e0b45b70b969
RicardoSampaio12121 <sampaio-_-2000@hotmail.com>
2020-11-13 10:48:52 +0000

    Sucessfull tests done

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ tokens = ("NUMBEROFTESTS", "SUCESSFULLTESTS")
 
 t_NUMBEROFTESTS = r"[1][.][.][0-9]+"
 
-# t_SUCESSFULLTESTS = r""
+t_SUCESSFULLTESTS = r"[o][k][ ][0-9]+"
 
 
-t_ignore = " "
+t_ignore = "\n"
 
 lexer = lex.lex()
 
@@ -20,18 +20,23 @@ lexer.input(f.read())
 
 
 # print(FTests)
-
+numberOfSucessfullTests = 0
 while True:
     tok = lexer.token()
     if not tok:
         break
     else:
-        # print(tok.value)
-        i = 0
-        NTests = ""
-        while i < len(tok.value):
-            if i > 2:
-                NTests += tok.value[i]
-            i += 1
-        FTests = int(NTests)
-        print("Number of tests: ", FTests)
+        if tok.value[0] == "1":
+            i = 0
+            NTests = ""
+            while i < len(tok.value):
+                if i > 2:
+                    NTests += tok.value[i]
+                i += 1
+            FTests = int(NTests)
+            print("Number of tests: ", FTests)
+        elif tok.value[0] == "o":
+            numberOfSucessfullTests += 1
+print("Sucessfull tests: ", numberOfSucessfullTests)
+
+
