1050318404dc01abacf58745422d66412ec2166e
RicardoSampaio12121 <sampaio-_-2000@hotmail.com>
2020-11-22 18:39:37 +0000

    Organizing code

--- html.py
+++ html.py
@@ -3,101 +3,109 @@ from utils import Utils
 
 class Html(object):
 
+    #Creates the first part of the Main.html file
     def CreateHtmlMain(self):
         main_string =  ""
         main_string += "<body style=\"background-color:#b0e0e6\">\n"
         main_string += "<h1>INDEX</h1>\n"
         return main_string
 
-    
+    #Adds the hyperlink to the other documents
     def AddHtmlLineHyperLink(self, main_string, path, file):
         main_string += "\t<a href= " + path + ".html" +">" + file +"</a><br>"
         
         return main_string
 
-
+    #Finnished the Main.html file
     def CloseHtmlMain(self, main_string):
         main_string += "</body>\n"
         return main_string
 
-    def CreateHtmlString(self, file_number, successful_tests_list, line_number, previous, next, comments, success, total_tests):
+    #Creates the html documents
+    def CreateHtmlString(self, file_name, successful_tests_list, previous, next, comments, success, total_tests):
         successTests = ((success / total_tests) * 100) * 3.6
 
-
-        html_string = "None"
+        #Header of the file
         html_string = "<head>\n"
         html_string += "<style> table, th, td{border: 1px solid black;}\n"
         html_string += """.piechart{margin-top: 20px; display: block; position: absolute; width: 400px; 
                         height: 400px; border-radius: 50%; background-image: conic-gradient( lightgreen """ + str(successTests) + """deg,  red 0deg); }\n"""
         html_string += "body, .piechart { display: outside; justify-content: center; align-items: center; }\n"
-        
         html_string += "</style>\n"
         html_string += "</head>\n"
 
+        #Checks if it has to add a previous and or a next button
         if previous != "noPrevious":
             html_string += "<a href= " + previous[:-4] + ".html" +">" + "---Previous" +"</a><br>"
         if next != "noNext":
             html_string += "<a href= " + next[:-4] + ".html" +">" + "Next>>>" +"</a><br>"
-            
+
+        #Changes the background color to light blue 
         html_string += "<body style=\"background-color:#b0e0e6\">\n"
-        html_string += "\t<h1> Relatório " + str(file_number) + "!</h1>\n"
         
+        #Title
+        html_string += "\t<h1> Relatório " + file_name + "!</h1>\n"
+        
+        #Creates a table and add two columns, Teste and Status
         html_string += """\t<table style="width:100%">\n"""
         html_string += "\t\t<tr style=\"background-color:#ffa500\">\n"
         html_string += "\t\t\t<th style=\"text-align:left\">Teste</th>\n"
         html_string += "\t\t\t<th style=\"text-align:left\">Status</th>\n"
         html_string += "\t\t</tr>\n"
         
-        
-
+        #Goes through a list of successful tests to add the value to the trable
         for test in successful_tests_list:
+            
+            #Gets the status and the number of the test from the token
             status = Utils.GetStatusFromToken(None, test)
             number = Utils.GetNumberFromToken(None, test)
             
-
+            #If status is ok then changes the color of the row to green
             if status == "ok":
                 html_string += "\t\t<tr style=\"background-color:#32CD32\">\n"
+            #Else to red
             else:
                 html_string += "\t\t<tr style=\"background-color:#FF0000\">\n"
                 
-            
+            #Fills the row spaces with the number and status
             html_string += "\t\t\t<th style=\"text-align:left\">" + number + "</th>\n"
             html_string += "\t\t\t<th style=\"text-align:left\">" + status + \
                             "</th>\n"
-
+            #Closes the row
             html_string += "\t\t</tr>\n"
 
+        #Closes table
         html_string += """\t</table>\n"""
 
-        
-        html_string += """\t<table style="width:100%">\n"""
-
-
+        #If the list of comments is not empty enters here
         if comments:
-
+            
+            #Creates a table and a title row
+            html_string += """\t<table style="width:100%">\n"""    
             html_string += "\t\t<br><tr style=\"background-color:#ffa500\">\n"
             html_string += "\t\t\t<th style=\"text-align:left;background-color:\">Comentários</th>\n"
             html_string += "\t\t</tr>\n"
 
-            
-
+            #Goes through the list of comments and fills the table with them
             for comment in comments:
                 html_string += "\t\t<tr style=\"background-color:#32CD32\">\n"
-                html_string += "\n\t\t\t<th style=\"text-align:left\">" + comment.value + "</th>"
-                             
+                html_string += "\n\t\t\t<th style=\"text-align:left\">" + comment.value + "</th>"                 
                 html_string += "</tr>"
+            
+            #Closes the table
+            html_string += """\t</table>\n"""
 
-        html_string += """\t</table>\n"""
-
-        html_string += "<h1>Percentagem de sucesso</h1>"
+        #Displays the pie chart with the percentage of success marked as green
+        #And unsuccess as red
+        html_string += "<h1>Pie-Chart Success</h1>"
         html_string += """<div class="piechart"></div>"""
- 
 
-
+        #Closes the body of the document
         html_string += "</body>"
 
         return html_string
 
+    #Converts the html documents streing to actual .html documents
     def HtmlStringToHtml(self, html_string, doc_name):
         if not os.path.exists("HTML_Documents"):
             os.mkdir("HTML_Documents")
--- main.py
+++ main.py
@@ -7,29 +7,28 @@ from utils import Utils
 
 tokens = ("NUMBEROFTESTS", "SUCESSFULLTESTS", "NOTSUCESSFULLTESTS", "SUBTEST", "COMMENTS")
 
+#Declaring necessary vars
 numberOfSucessfullTests = 0
 numberOfNotSucessfullTests = 0
 nTests = 0
 subTestsEntering = 0
-numberOfSubtests = 0
 counterSub = 0
 numberOfSucessfullSubTests = 0
 numberOfSubtestsNotSucessFull = 0
-numberOfTests = 0
-numberOfSubTests2 = 0
+numberOfSubTests = 0
 
+#And lists
 listOfSucessfullTests = []
 comments = []
 
 
 def t_NUMBEROFTESTS(t):
     r"[1][.][.][0-9]+"
-    global subTestsEntering, numberOfSubtests, nTests, counterSub
+    global subTestsEntering, nTests, counterSub
 
     if subTestsEntering == 0:
         nTests = int(t.value[3:])
     else:
-        numberOfSubtests += int(t.value[3:])
         subTestsEntering -= 1
         counterSub += int(t.value[3:])
 
@@ -39,16 +38,16 @@ def t_NUMBEROFTESTS(t):
 def t_SUCESSFULLTESTS(t):
     r"ok[ ][0-9]+([ ][-#][ ].+)?"
 
-    global numberOfSucessfullTests, numberOfSucessfullSubTests, listOfSucessfullTests, counterSub, numberOfTests
+    global numberOfSucessfullTests, numberOfSucessfullSubTests, listOfSucessfullTests, counterSub, numberOfSubTests
 
     listOfSucessfullTests.append(t)
 
     if subTestsEntering == 0 & counterSub == 0:
         numberOfSucessfullTests += 1
-        numberOfTests += 1
 
     else:
         numberOfSucessfullSubTests += 1
+        numberOfSubTests += 1
         counterSub -= 1
 
     return t
@@ -57,15 +56,15 @@ def t_SUCESSFULLTESTS(t):
 def t_NOTSUCESSFULLTESTS(t):
     r"not[ ]ok[ ][0-9]+([ ][-#][ ].+)?"
 
-    global numberOfNotSucessfullTests, numberOfSubtestsNotSucessFull, counterSub, listOfSucessfullTests, numberOfTests
+    global numberOfNotSucessfullTests, numberOfSubtestsNotSucessFull, counterSub, listOfSucessfullTests, numberOfSubTests
     listOfSucessfullTests.append(t)
 
     if subTestsEntering == 0 & counterSub == 0:
         numberOfNotSucessfullTests += 1
-        numberOfTests += 1
 
     else:
         numberOfSubtestsNotSucessFull += 1
+        numberOfSubTests += 1
         counterSub -= 1
 
     return t
@@ -74,9 +73,8 @@ def t_NOTSUCESSFULLTESTS(t):
 def t_SUBTEST(t):
     r"[#][ ]Subtest[:].+"
 
-    global subTestsEntering, numberOfSubTests2
+    global subTestsEntering
     subTestsEntering += 1
-    numberOfSubTests2 += 1
 
     return t
 
@@ -96,9 +94,12 @@ def t_error(t):
 
 
 lexer = lex.lex()
+
+#Get the file/Folder from terminal/console
 file = sys.argv[1]  
-f = ""
+
 _isFile = False
+
 # If it gets a file as an argument
 try:
     f = open("Testes/" + file, "r")
@@ -107,88 +108,92 @@ except:
     pass
 
 # If it gets a folder as an argument 
-teste3 = ""
 if _isFile == False:
-    teste4 = Html.CreateHtmlMain(None)
+    htmlMain = Html.CreateHtmlMain(None)
     i = 0
-    previous_link = ""
-    next_link = ""
-    amount_fo_files = 0
-    for file in os.listdir(sys.argv[1]): amount_fo_files += 1 
+    amount_of_files = 0
 
+    #Counts the amount of files in the directory
+    for file in os.listdir(sys.argv[1]): amount_of_files += 1 
+
+    #Goes throught the files inside the directory
     for file in os.listdir(sys.argv[1]):
         i += 1
         t = open(sys.argv[1] + "/" + file, "r")
         
         lexer.input(t.read())
         
-        
+        #Goes through every token
         for token in iter(lexer.token, None):
             pass
-
-        testNumber = Utils.GetNumberOfFile(None, file)
-
+        
+        #Checks if it's the first file in process
+        #If it is then the html of the documents
+        #Won't have a previous file button
         if i == 1:
             previous_link = "noPrevious"
             next_link = Utils.NextFileInDirectory(None, sys.argv[1], file)
         
-        elif i == amount_fo_files:
+        #If it is the last file
+        #The next button won't do anything
+        elif i == amount_of_files:
             previous_link = Utils.PreviousFileInDirectory(None, sys.argv[1], file)
             mext_link = "noNext"
 
+        #If it's between, then they'll work just fine
         else: 
             previous_link = Utils.PreviousFileInDirectory(None, sys.argv[1], file)
             next_link = Utils.NextFileInDirectory(None, sys.argv[1], file)
 
-
-        teste3 = Html.CreateHtmlString(None, testNumber, listOfSucessfullTests, i, previous_link, next_link, comments, numberOfSucessfullSubTests+numberOfSucessfullTests, numberOfTests + numberOfSubTests2)
+        #Builds the html document in a string
+        htmlString = Html.CreateHtmlString(None, file[:-4], listOfSucessfullTests, previous_link, next_link, comments, numberOfSucessfullSubTests+numberOfSucessfullTests, nTests + numberOfSubTests)
         
+        #Converts the html in the string to a .html document
+        Html.HtmlStringToHtml(None, htmlString, file[:-4])
         
-        Html.HtmlStringToHtml(None, teste3, file[:-4])
+        #Adds an hyperlink to the document created in the previous line in the main .html file
+        htmlMain = Html.AddHtmlLineHyperLink(None, htmlMain, file[:-4], file[:-4])
         
-        teste4 = Html.AddHtmlLineHyperLink(None, teste4, file[:-4], file[:-4])
         t.close()
 
         #Print report to the terminal/console
-        Utils.PrintReport(None, nTests, numberOfSucessfullTests, numberOfSubTests2, numberOfSucessfullSubTests)
+        Utils.PrintReport(None, nTests, numberOfSucessfullTests, numberOfSubTests, numberOfSucessfullSubTests)
 
-
+        #Resets all the vars being used in this loop
         listOfSucessfullTests.clear()
         comments.clear()
         nTests = 0
         numberOfSucessfullTests = 0
         numberOfSucessfullSubTests = 0
-        numberOfTests = 0
-        numberOfSubTests2 = 0
+        numberOfSubTests = 0
 
+    #Closes the body of the main .html file
+    htmlMain = Html.CloseHtmlMain(None, htmlMain)
+    #Converts the main string to a .html
+    Html.HtmlStringToHtml(None, htmlMain, "Main")
 
-        #Relatorio terminal
-
-
-
-
-
-    teste4 = Html.CloseHtmlMain(None, teste4)
-    Html.HtmlStringToHtml(None, teste4, "Main")
-
+#If the input is a file
 else:
     lexer.input(f.read())
+    
+    #Goes through the tokens
     for token in iter(lexer.token, None): 
         pass
-
-    testNumber = Utils.GetNumberOfFile(None, file)
-    teste3 = None
-    teste3 = Html.CreateHtmlString(None, testNumber, listOfSucessfullTests)
     
-    Html.HtmlStringToHtml(None, teste3, file[:-4])
+    #Builds the html document in a string
+    htmlString = Html.CreateHtmlString(None, file[:-4], listOfSucessfullTests, "noPrevious", "noNext", comments, numberOfSucessfullSubTests+numberOfSucessfullTests, nTests + numberOfSubTests)
+    
+    #Converts the string to a .html
+    Html.HtmlStringToHtml(None, htmlString, file[:-4])
     f.close()
 
-#print("Number of main tests:", nTests)
-#print("Sucessfull tests:", numberOfSucessfullTests)
-#print("Percentage of not sucessfull test:", (numberOfNotSucessfullTests / nTests) * 100, "%")
-#print("Number Subtests: ", numberOfSubtests)
-#print("Sucessfull Subtests: ", numberOfSucessfullSubTests)
-#if numberOfSubtests > 0:
-#    print("Percentage of not sucessfull subtests:", numberOfSubtestsNotSucessFull / numberOfSubtests * 100, "%")
-#else:
-#    print("Percentage of not sucessfull subtests: 0%")
+    #Print report
+    Utils.PrintReport(None, nTests, numberOfSucessfullTests, numberOfSubTests, numberOfSucessfullSubTests)
+
+    #Resets the vars
+    listOfSucessfullTests.clear()
+    comments.clear()
+    nTests = 0
+    numberOfSucessfullTests = 0
+    numberOfSucessfullSubTests = 0
+    numberOfSubTests = 0
